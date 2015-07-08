3b7e479e3dd735a2ad29d7548e1591d57740f6ea
vitaut <victor.zverovich@gmail.com>
2015-07-08 12:05:39 -0700

    Report errors

--- src/com/example/transp/ErrorHandler.java
+++ src/com/example/transp/ErrorHandler.java
@@ -2,14 +2,14 @@ package com.example.transp;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-//import org.eclipse.ui.statushandlers.StatusManager;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.osgi.framework.FrameworkUtil;
 
 public class ErrorHandler {
 	public void handle(String errorMessage) {
 		String id = FrameworkUtil.getBundle(ErrorHandler.class).getSymbolicName();
-		//StatusManager.getManager().handle(
-		//		new Status(IStatus.ERROR, id, errorMessage), StatusManager.SHOW);
+		ErrorDialog.openError(null, "Error", null,
+				new Status(IStatus.ERROR, id, errorMessage));
 	}
 
 	public void handle(Exception e) {
